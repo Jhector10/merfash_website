260a8e20861d612d1d6f4c891c45b16c8a056779
Sylvia Boamah <60356081+sboamah@users.noreply.github.com>
2023-06-11 23:20:57 -0400

    routing

--- merfash-website/src/app/app-routing.module.ts
+++ merfash-website/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
