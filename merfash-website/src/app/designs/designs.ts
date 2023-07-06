51f0f3ecb94069e8e71172c1f372f09a07e7cd6a
Sylvia Boamah <sbsboamah@gmail.com>
2023-07-06 15:08:24 -0400

    Readd files

---
+++ merfash-website/src/app/designs/designs.ts
@@ -0,0 +1,99 @@
+export class Designs{
+    id!: number;
+    img = '';
+    alt = '';
+}
+
+export const DESIGNS: Designs[] = [
+    {
+        id: 1,
+        img: 'assets/blackbodycon.jpg',
+        alt: 'Black Bodycon dress'
+    },
+    {
+        id: 2,
+        img: 'assets/grey.jpg',
+        alt: 'Grey dress'
+    },
+    {
+        id: 3,
+        img: 'assets/bluefit.JPG',
+        alt: 'Blue Outfit'
+    },
+    {
+        id: 4,
+        img: 'assets/wedding-haute-couture.jpg',
+        alt: 'Wedding couture dress'
+    },
+    {
+        id: 5,
+        img: 'assets/gold.jpg',
+        alt: 'Gold dress'
+    },
+    {
+        id: 6,
+        img: 'assets/mercedes-santana-designer-haute-couture-6-scaled.jpg',
+        alt: 'Birthday outfit'
+    },
+    {
+        id: 7,
+        img: 'assets/lightpink.jpg',
+        alt: 'Light pink dress'
+    },
+    {
+        id: 8,
+        img: 'assets/mercedes-santana-designer-haute-couture-9.jpg',
+        alt: 'Trench coat outfit'
+    },
+    {
+        id: 9,
+        img: 'assets/nyfashionshow1.PNG',
+        alt: 'NYFW dress 1'
+    },
+    {
+        id: 10,
+        img: 'assets/nyfashionshow2.PNG',
+        alt: 'NYFW dress 2'
+    },
+    {
+        id: 11,
+        img: 'assets/nyfashionshow3.PNG',
+        alt: 'NYFW dress 3'
+    },
+    {
+        id: 12,
+        img: 'assets/pinkfront.jpg',
+        alt: 'Pink dress'
+    },
+    {
+        id: 13,
+        img: 'assets/nyfashionshow5.jpg',
+        alt: 'NYFW dress 5'
+    },
+    {
+        id: 14,
+        img: 'assets/WeddingDress.jpeg',
+        alt: 'Wedding Dress'
+    },
+    {
+        id: 15,
+        img: 'assets/nyfashionshow14.jpg',
+        alt: 'NYFW dress 14'
+    },
+    {
+        id: 16,
+        img: 'assets/sleeveless.jpg',
+        alt: 'Sleeveless dress'
+    },
+    {
+        id: 17,
+        img: 'assets/yellow1.jpg',
+        alt: 'Yellow dress'
+    },
+    {
+        id: 18,
+        img: 'assets/purplewithstairs.jpg',
+        alt: 'Purple prom dress'
+    },
+    
+]
