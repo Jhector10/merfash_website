a62e38cbd2993eb74e2a81d9a3543b211bf2a64e
Jhector10 <jashsmash10@gmail.com>
2023-07-24 19:52:42 -0400

    added in a video testimonial and some more pictures in designs

--- merfash-website/src/app/homepage/testimonials/testimonials.ts
+++ merfash-website/src/app/homepage/testimonials/testimonials.ts
@@ -8,13 +8,30 @@ export class Testimonials{
 }
 
 export const TESTIMONIALS: Testimonials[] = [
-    {
-        id: 1,
-        quote: 'Mercedes is amazing in transforming dresses into something truly special. She did a beautiful job for my wedding. Her attention to detail is truly a gift.',
-        img: 'assets/testimonials-1.jpg',
-        alt: 'Picture of testimonial',
-        name: 'Ericka',
-        role: 'Client'
-    },
+    
+    // {
+    //     id: 1,
+    //     quote: 'Mercedes is amazing in transforming dresses into something truly special. She did a beautiful job for my wedding. Her attention to detail is truly a gift.',
+    //     img: 'assets/testimonials-1.jpg',
+    //     alt: 'Picture of testimonial',
+    //     name: 'Ericka',
+    //     role: 'Client'
+    // },
+    // {
+    //     id: 2,
+    //     quote: 'Mercedes is amazing in transforming dresses into something truly special. She did a beautiful job for my wedding. Her attention to detail is truly a gift.',
+    //     img: 'assets/testimonials-1.jpg',
+    //     alt: 'Picture of testimonial',
+    //     name: 'Ericka',
+    //     role: 'Client'
+    // },
+    // {
+    //     id: 3,
+    //     quote: 'Mercedes is amazing in transforming dresses into something truly special. She did a beautiful job for my wedding. Her attention to detail is truly a gift.',
+    //     img: 'assets/testimonials-1.jpg',
+    //     alt: 'Picture of testimonial',
+    //     name: 'Ericka',
+    //     role: 'Client'
+    // },
 
 ]
