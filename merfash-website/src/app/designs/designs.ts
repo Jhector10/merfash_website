ed6bdf6a92868e8ec8d359f93e3617da0a1788f1
Jhector10 <jashsmash10@gmail.com>
2024-06-30 09:55:30 -0400

    update nj moda to be the focal point of the designs page

--- merfash-website/src/app/designs/designs.ts
+++ merfash-website/src/app/designs/designs.ts
@@ -7,108 +7,177 @@ export class Designs{
 export const DESIGNS: Designs[] = [
     {
         id: 1,
-        img: 'assets/Emily-ratajkowski-at-met-gala.jpg',
-        alt: 'Emily Ratajkowski at Met Gala'
-    },
-    {
-        id: 2,
-        img: 'assets/two-dresses.jpg',
+        img: 'assets/nj_moda/nj-moda-1.jpg',
         alt: 'Two custom wedding dresses (multiple iterations)'
     },
     {
-        id: 3,
-        img: 'assets/greenblack.jpg',
+        id: 2,
+        img: 'assets/nj_moda/nj-moda-2.jpg',
         alt: 'Green and Black outfit'
     },
     {
-        id: 4,
-        img: 'assets/beigesuite.jpg',
+        id: 3,
+        img: 'assets/nj_moda/nj-moda-3.jpg',
         alt: 'Beige Suit'
     },
     {
-        id: 5,
-        img: 'assets/Gold-around-the-met-gala.jpg',
+        id: 4,
+        img: 'assets/nj_moda/nj-moda-4.jpg',
         alt: 'Gold dress'
     },
     {
-        id: 6,
-        img: 'assets/weddingtracksfront.jpg',
+        id: 5,
+        img: 'assets/nj_moda/nj-moda-5.jpg',
         alt: 'Wedding Dress'
     },
     {
-        id: 7,
-        img: 'assets/twotoneblue.jpg',
+        id: 6,
+        img: 'assets/nj_moda/nj-moda-6.jpg',
         alt: 'Two tone blue outfit'
     },
     {
-        id: 8,
-        img: 'assets/whitefit.jpg',
+        id: 7,
+        img: 'assets/nj_moda/nj-moda-7.jpg',
         alt: 'White outfit'
     },
     {
-        id: 9,
-        img: 'assets/greywithouther.jpg',
+        id: 8,
+        img: 'assets/nj_moda/nj-moda-8.jpg',
         alt: 'Grey dress'
     },
     {
-        id: 10,
-        img: 'assets/beigedown.jpg',
+        id: 9,
+        img: 'assets/nj_moda/nj-moda-9.jpg',
         alt: 'Beige outfit'
     },
     {
-        id: 11,
-        img: 'assets/blueonbluesuit.jpg',
+        id: 10,
+        img: 'assets/nj_moda/nj-moda-10.jpg',
         alt: 'Blue outfit'
     },
     {
+        id: 11,
+        img: 'assets/nj_moda/nj-moda-11.jpg',
+        alt: 'Red white and blue outfit'
+    },
+    {
         id: 12,
-        img: 'assets/redwhiteblue.jpg',
+        img: 'assets/nj_moda/nj-moda-12.jpg',
         alt: 'Red white and blue outfit'
     },
     {
         id: 13,
-        img: 'assets/blackdetails.jpg',
+        img: 'assets/nj_moda/nj-moda-13.jpg',
         alt: 'Black detail dress'
     },
     {
         id: 14,
-        img: 'assets/pinkfront.jpg',
+        img: 'assets/nj_moda/nj-moda-14.jpg',
         alt: 'Pink dress',
     },
     {
         id: 15,
-        img: 'assets/greenwithshoulders.jpg',
+        img: 'assets/nj_moda/nj-moda-15.jpg',
         alt: 'Green dress with shoulders'
     },
     {
         id: 16,
-        img: 'assets/pinkig.jpg',
+        img: 'assets/nj_moda/nj-moda-16.jpg',
         alt: 'Pink ruffle dress'
     },
     {
         id: 17,
-        img: 'assets/bluestreaks.jpg',
+        img: 'assets/nj_moda/nj-moda-17.jpg',
         alt: 'Blue streak dress'
     },
     {
         id: 18,
-        img: 'assets/whitepoofy.jpg',
+        img: 'assets/nj_moda/nj-moda-18.jpg',
         alt: 'White poofy dress'
     },
     {
         id: 19,
-        img: 'assets/blackbodycon.jpg',
+        img: 'assets/nj_moda/nj-moda-19.jpg',
         alt: 'Black Bodycon dress'
     },
     {
         id: 20,
-        img: 'assets/grey.jpg',
+        img: 'assets/nj_moda/nj-moda-20.jpg',
         alt: 'Grey dress'
     },
     {
         id: 21,
-        img: 'assets/giselle-white.jpg',
+        img: 'assets/nj_moda/nj-moda-21.jpg',
         alt: 'Recent white ruffles dress'
     },
-    
+    {
+        id: 22,
+        img: 'assets/nj_moda/nj-moda-22.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 23,
+        img: 'assets/nj_moda/nj-moda-23.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 24,
+        img: 'assets/nj_moda/nj-moda-24.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 25,
+        img: 'assets/nj_moda/nj-moda-25.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 26,
+        img: 'assets/nj_moda/nj-moda-26.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 27,
+        img: 'assets/nj_moda/nj-moda-27.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 28,
+        img: 'assets/nj_moda/nj-moda-28.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 29,
+        img: 'assets/nj_moda/nj-moda-29.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 30,
+        img: 'assets/nj_moda/nj-moda-30.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 31,
+        img: 'assets/nj_moda/nj-moda-31.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 32,
+        img: 'assets/nj_moda/nj-moda-32.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 33,
+        img: 'assets/nj_moda/nj-moda-33.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 34,
+        img: 'assets/nj_moda/nj-moda-34.jpg',
+        alt: 'Recent white ruffles dress'
+    },
+    {
+        id: 35,
+        img: 'assets/nj_moda/nj-moda-35.jpg',
+        alt: 'Recent white ruffles dress'
+    },
 ]
